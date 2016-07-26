c345aba98afb8a24e355bece5f25dedb22afc783
burning0406 <1016368923@qq.com>
2016-07-26 23:41:55 +0800

    提交分类

---
+++ Category/获取文本框的占位文字及修改文字属性/UITextField+Placeholder.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+Placeholder.h
+//  01-BuDeJie
+//
+//  Created by hqc on 16/1/7.
+//  Copyright © 2016年 hqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (Placeholder)
+
+@property UIColor *placeholderColor;
+- (void)setPlaceholder:(NSString *)placeholder;
+@end
