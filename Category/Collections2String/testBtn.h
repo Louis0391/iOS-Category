afc3898ffc96150ff75a6da8b45ddf43031bc62b
burning0406 <1016368923@qq.com>
2016-11-13 16:13:24 +0800

    test

---
+++ Category/Collections2String/testBtn.h
@@ -0,0 +1,13 @@
+//
+//  testBtn.h
+//  Category
+//
+//  Created by hqc on 16/11/13.
+//  Copyright © 2016年 hqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testBtn : UIButton
+
+@end
---
+++ Category/Collections2String/testBtn.m
@@ -0,0 +1,21 @@
+//
+//  testBtn.m
+//  Category
+//
+//  Created by hqc on 16/11/13.
+//  Copyright © 2016年 hqc. All rights reserved.
+//
+
+#import "testBtn.h"
+
+@implementation testBtn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
